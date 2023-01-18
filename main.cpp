46eda25de6214d9eee1141f13791783b20c04eb6
yaros <yaroslavdisiak@gmail.com>
2023-01-18 17:54:49 +0200

    ex3donepw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void Exercise(){
+    int firstm,secondm;
+    int res =0;
+    cout <<"Enter first limit: ";
+    cin >> firstm;
+    cout <<"Enter second limit: ";
+    cin >> secondm;
+    for (int i = firstm; i <= secondm; i++)
+    {
+        res += i;
+    }
+    cout << "Suma:  " << res << endl;
+    
+}
+int main(){
+    Exercise();
+    return 0;
+}
