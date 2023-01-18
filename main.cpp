be06a0877066ff1e389d1a8d33b87f77d5113ac3
yaros <yaroslavdisiak@gmail.com>
2023-01-18 22:45:23 +0200

    ex4donepw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 
+
+#include <iostream>
+using namespace std;
+void Exercise(){
+
+    int num, sum = 0;
+    cout << "Enter numbers, enter 0 to stop: ";
+    cin >> num;
+    while (num != 0) {
+        sum += num;
+        cin >> num;
+    }
+    cout<< "Suma: "<<sum;
+}
+int main(){
+    Exercise();
+    return 0;
+}
