c11c51b68b11cc2aaef22ddd36de1c24288f5bbc
yaros <yaroslavdisiak@gmail.com>
2023-01-18 16:58:23 +0200

    ex1donepw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int num;
+int null = 0;
+
+
+void Exercise() {
+    cout << "Enter number: ";
+    cin>> num; //числа яке вводить користувач
+    do
+    {
+        cout << null<<" ";
+        ++null;
+
+    } while (null <= num);// умова що число додається до i до поки не буде рівне з num
+
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
