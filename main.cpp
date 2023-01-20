4703b6fc0ec34d027b6941cb63803657481ae12f
yaros <yaroslavdisiak@gmail.com>
2023-01-20 20:59:30 +0200

    ex6donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,38 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void Exercise() {
+    int n;
+    for (int i = 100; i <=999 ; ++i) {
+        if(i/10%10 == i%10){
+            ++n;
+
+        }
+        else if(i/100%10==i/10%10){
+            ++n;
+
+        }
+        else if(i/100 %10==i%10){
+            ++n;
+
+        }
+        else{
+            continue;
+        }
+
+    }
+    cout <<"Kolvo chisel: "<< n;
+
+
+    //числа яке вводить користувач 1.if(i/10%10 == i%10) 2.if(i/100%10==i/10%10) 3.if(i/100 %10==i%10)
+
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
