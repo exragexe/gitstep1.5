f92758f1a98afe1afcc94d24b2c97305e172e47a
yaros <yaroslavdisiak@gmail.com>
2023-01-20 21:24:07 +0200

    ex8donecpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void Exercise() {
+    int num;
+    cin >> num;
+    int new_num = 0;
+    int digit, place = 1;
+    while (num > 0) {
+        digit = num % 10;
+        if (digit != 3 && digit != 6) {
+            new_num += digit * place;
+            place *= 10;
+        }
+        num /= 10;
+    }
+    cout << new_num << endl;
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
