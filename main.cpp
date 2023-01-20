d269f48f84bb28477f4cfd2e245c89887ba65f45
yaros <yaroslavdisiak@gmail.com>
2023-01-20 22:11:54 +0200

    ex11donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+
+
+void Exercise() {
+    int num;
+    cout <<"Enter number: ";
+    cin >>num;
+    for (int n = 1; n<=num; ++n) {
+        if(num%n ==0){
+            cout <<n<<" ";
+        }
+        else{
+            cout <<"";
+        }
+
+
+    }
+
+
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
