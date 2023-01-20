fc25ba299551fef19cc47f4d0f957154f30331f3
yaros <yaroslavdisiak@gmail.com>
2023-01-20 17:51:30 +0200

    ex3donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+void Exercise() {
+    int one=0;
+    int sto = 1000;
+    int res;
+    for (; one <= sto; ) {
+        res += ++one -1;
+    }
+
+    cout <<"AVG: "<<res / sto;
+
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
