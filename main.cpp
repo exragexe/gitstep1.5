4a3e98c5e442b8be89aaba7c76f85dc212dfdb5d
yaros <yaroslavdisiak@gmail.com>
2023-01-20 21:48:44 +0200

    ex9donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+void Exercise(){
+
+    int A;
+    cout <<"Enter number: ";
+    cin >> A;
+    for (int B = 1; B <= A; B++) {
+        if (A % (B*B) == 0 && A % (B*B*B) != 0) {
+            cout << B << " ";
+        }
+    }
+    cout << endl;
+
+
+}
+int main(){
+    Exercise();
+    return 0;
+}
