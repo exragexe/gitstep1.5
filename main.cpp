11ee6ee4367cbf97704565e013ef00668a56fda3
yaros <yaroslavdisiak@gmail.com>
2023-01-20 21:58:57 +0200

    ex10cpphw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+void Exercise(){
+
+    int A;
+    cout <<"Enter number: ";
+    cin >> A;
+    int sum =0;
+    while(A>0){
+        sum += A % 10;
+        A /= 10;
+    }
+    if(sum*sum*sum == A*A){
+        cout <<"True";
+    }
+    else{
+        cout <<"False";
+    }
+}
+int main(){
+    Exercise();
+    return 0;
+}
