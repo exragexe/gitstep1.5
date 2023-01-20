a8c9ee3f0e0cace09a0ca0ddf4f6a7ed0d828bc6
yaros <yaroslavdisiak@gmail.com>
2023-01-20 20:58:56 +0200

    ex5donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void Exercise() {
+    int num,res;
+    cout << "Enter number: ";
+    cin>> num; //числа яке вводить користувач
+    for (int i = 1; i <=10 ; ++i ) {
+        res = num * i;
+        cout << num<<"*"<<i<<" = "<<res<<endl;
+
+    }
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
