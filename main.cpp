aef41562215a29e82b6fce74b2653086ffa08e2c
yaros <yaroslavdisiak@gmail.com>
2023-01-20 17:13:20 +0200

    ex2donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+
+
+void Exercise() {
+    int x,y;
+
+    cout << "Enter first number: ";
+    cin>> x;
+    cout <<"Enter second number: ";
+    cin >> y;
+
+    cout <<x<<" in degrees "<< y << " = "<< pow(x,y);
+
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
