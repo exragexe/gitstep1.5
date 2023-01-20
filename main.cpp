700e344e137af50a48c468bee619d881d01d6bbc
yaros <yaroslavdisiak@gmail.com>
2023-01-20 20:57:07 +0200

    ex4donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void Exercise() {
+    long long num,res=1;
+    const int num2 = 20;
+    cout << "Enter number: ";
+    cin>> num; //числа яке вводить користувач
+    for (int i = num; i <= num2 ; ++i ) {
+        if(i>20){
+            break;
+        }
+        res *= i;
+    }
+    cout << "Dobytok: "<< res;
+
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
