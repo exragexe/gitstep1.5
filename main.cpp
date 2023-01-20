3a3bedf7c66ff9a665d5a667175762f91e54e430
yaros <yaroslavdisiak@gmail.com>
2023-01-20 22:16:02 +0200

    ex12donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+
+
+void Exercise() {
+    int num,num2;
+    cout <<"Enter first number: ";
+    cin >>num;
+    cout <<"Enter second number: ";
+    cin >>num2;
+    for (int n = 1; n<=num; ++n) {
+        if(num%n ==0&&num2%n==0){
+            cout <<n<<" ";
+        }
+        else{
+            cout <<"";
+        }
+
+
+    }
+
+
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
